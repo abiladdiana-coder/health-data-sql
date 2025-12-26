4fe774973fbfeb7d321786c4e865faeaafb7727a
abiladdiana-coder <abilad.diana@gmail.com>
2025-12-26 17:51:20 +0100

    Fix SQL syntax for counting adverse events

--- 02_device_adverse_events/scripts/02_top_5_manufacturers_with_highest_number_of_events.sql
+++ 02_device_adverse_events/scripts/02_top_5_manufacturers_with_highest_number_of_events.sql
@@ -1,4 +1,4 @@
-SELECT manufacturer, COUNT (*) AS total_adverse events
+SELECT manufacturer, COUNT (*) AS total_adverse_events
 FROM maude_2023
 GROUP BY manufacturer
 ORDER BY total_adverse_events DESC
